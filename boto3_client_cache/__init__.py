a8d459b3c0362d4b34c9cede7dcfa818fe54c9a8
Mike Letts <47342651+michaelthomasletts@users.noreply.github.com>
2026-02-15 03:06:00 -0500

    fix: Launch to PyPI

--- boto3_client_cache/__init__.py
+++ boto3_client_cache/__init__.py
@@ -6,3 +6,8 @@ from .exceptions import *  # noqa: F403
 
 __all__ += cache.__all__
 __all__ += exceptions.__all__
+__title__ = "boto3-client-cache"
+__author__ = "Mike Letts"
+__maintainer__ = "Mike Letts"
+__license__ = "Apache License 2.0"
+__email__ = "lettsmt@gmail.com"

14d0b9b8ca9ca7a09fdc3d7c4b9b37a79f9b1d07
Mike Letts <47342651+michaelthomasletts@users.noreply.github.com>
2026-02-27 18:07:15 -0500

    fix! Adding description to __init__.py

--- boto3_client_cache/__init__.py
+++ boto3_client_cache/__init__.py
@@ -2,6 +2,10 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+"""A concurrency-safe, bounded cache for boto3 clients and resources with
+deterministic identity semantics.
+"""
+
 __all__ = []
 
 from . import cache, exceptions, session
